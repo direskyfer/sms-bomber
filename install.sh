8ebcabd85d746ca77733b470cfc1b01050526e95
direskyfer <78743691+direskyfer@users.noreply.github.com>
2021-03-31 12:46:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash/
+apt install python3
 apt install python3-pip -y
 pip install requests
 pip install colorama
+pip3 install requests
+pip3 install colorama
+apt install python
 ls
-echo start bomber
+echo "start bomber"
 python3 bomber.py
